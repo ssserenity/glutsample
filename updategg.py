222b4cb837b1be7e92474659f37379c82541bc89
Kohe Tokoi <tokoik@gmail.com>
2013-06-26 22:37:51 +0900

    update updategg.py

--- updategg.py
+++ updategg.py
@@ -6,6 +6,8 @@ with open('glext.h', 'r') as f:
 	for line in f:
 		s = line.split()
 		if len(s) > 4 and s[0] == 'GLAPI':
+			while s[3][:2] != "gl":
+				s.pop(0)
 			funcs.append(s[3])
 
 with open('gg.h.inc', 'w') as f:
@@ -16,9 +18,7 @@ with open('gg.cpp.inc', 'w') as f:
 	for func in funcs:
 		f.write('PFN' + func.upper() + 'PROC ' + func + ';\n')
 	f.write('static PFNWGLSWAPINTERVALEXTPROC wglSwapIntervalEXT;\n')
-	f.write('\n#define GGGETPROCADDRESS(func) func = glfwGetProcAddress(#func)\n')
 	f.write('\nstatic inline void initGLExtFunc(void)\n{\n')
 	for func in funcs:
-		#f.write('  ' + func + ' = (PFN' + func.upper() + 'PROC)glfwGetProcAddress("' + func + '");\n')
-		f.write('  GGGETPROCADDRESS(' + func + ');\n')
+		f.write('  ' + func + ' = (PFN' + func.upper() + 'PROC)wglGetProcAddress("' + func + '");\n')
 	f.write('}\n')
